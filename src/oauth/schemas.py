c64918b9ab3887bd41c1fe6bf289f11805b13a36
Relanit <stulovmaxim3@gmail.com>
2023-10-10 23:29:31 +0300

    refactoring

--- src/oauth/schemas.py
+++ src/oauth/schemas.py
@@ -1,6 +1,8 @@
 from beanie import PydanticObjectId
 from fastapi_users import schemas
-from pydantic import BaseModel
+from pydantic import BaseModel, Field
+
+from oauth.models import OAuthAccount
 
 
 class Body(BaseModel):
@@ -12,12 +14,10 @@ class OAuthURL(BaseModel):
     authorization_url: str
 
 
-class Success(BaseModel):
-    success: bool
-
-
 class UserRead(schemas.BaseUser[PydanticObjectId]):
-    pass
+    avatar_url: str
+    display_name: str
+    connections: list[OAuthAccount] = Field(default_factory=list)
 
 
 class UserUpdate(schemas.BaseUserUpdate):
